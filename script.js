eb51783568bbfec3ae48de1952602db3a9696fc4
Tiago Pinheiro <tiago.plemes@gmail.com>
2025-02-22 11:49:27 -0300

    Adicionando HTML de conteúdo | Estilização também

--- script.js
+++ script.js
@@ -47,16 +47,31 @@ const saveUser = (userForm) => {
 
 const showInvite = (userForm) => {
     app.innerHTML = `
-        <input type="text" id="link" value="https://evento.com?ref=${userForm.ref}"
-         disabled>
-    
-        <div>
-            ${getTotalSubscribers(userForm)}
-        </div>
-        <h4>
-            <p>Inscrições feitas</p>
-        </h4>
+        <main>
+            <h3>Inscrição Confirmada</h3>
+            <p>
+                Convide mais pessoas e concorra a prêmios! <br/>
+                Compartilhe o link e acompanhe as inscrições:
+            </p>
+            <div class="input-group">
+                <label for="link">
+                    <img src="link.svg" alt="Link icon">
+                </label>
+                 <input type="text" id="link" value="https://evento.com?ref=${userForm.ref}"
+                    disabled>
+            </div>
+        </main>
+
+        <section class="stats">
+            <h4>
+                ${getTotalSubscribers(userForm)}
+            </h4>
+            <p>
+                inscrições feitas
+            </p>
+        </section>
     `
+    updateImageLogo()
 }
 
 const formAction = () => {
@@ -81,19 +96,70 @@ const formAction = () => {
     }
 }
 
+const updateImageLogo = () => {
+    document.querySelectorAll('img').forEach((img) => {
+        const src = img.getAttribute('src');  // Obtém o caminho original da imagem
+        const newSrc = `img/${src}`;  // Adiciona o caminho relativo da pasta img
+        img.setAttribute('src', newSrc);  // Substitui o src pela nova URL
+    })
+}
+
 const startApp = () => {
     const content = `
-    <form id="form">
-        <input type="name" name="name" placeholder="Seu nome">
-        <input type="email" name="email" placeholder="E-mail">
-        <input type="text" name="phone" placeholder="Telefone">
+    <main>
+            <section class="about">
+                <div class="section-header">
+                    <h2>
+                        Sobre o evento
+                    </h2>
+                    <span class="badge">AO VIVO</span>
+                </div>
 
-        <button>
-            Confirmar
-        </button>
-    </form>
+                <p>
+                    Um evento feito por e para pessoas desenvolvedoras 
+                    apaixonadas por criar soluções inovadoras e 
+                    compartilhar conhecimento. Vamos mergulhar nas 
+                    tendências mais recentes em desenvolvimento de 
+                    software, arquitetura de sistemas e tecnologias 
+                    emergentes, com palestras, workshops e hackathons.
+                    <br/><br/>
+                    Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito 
+                </p>
+            </section>
+
+            <section class="registration"> 
+                <h2>Inscrições</h2>
+
+                <form id="form">
+                    <div class="input-wrapper">
+                        <div class="input-group">
+                            <label for="email">
+                                <img src="mail.svg" alt="Email icon">
+                            </label>
+                            <input type="email" name="email" id="email"
+                                placeholder="E-mail">
+                        </div>
+
+                        <div class="input-group">
+                            <label for="phone">
+                                <img src="phone.svg" alt="Phone icon">
+                            </label>
+                            <input type="text" name="phone" id="phone"
+                                placeholder="Telefone">
+                        </div>
+
+                        <button>
+                            Confirmar
+                            <img src="arrow.svg" alt="Arrow right">
+                        </button>
+                    </div>
+                </form>
+            </section>
+        </main>
     `
     app.innerHTML = content;
+
+    updateImageLogo()
 
     formAction()
 }
